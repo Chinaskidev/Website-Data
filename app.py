62202e39e471801d5062afe48185c823f4f6992b
Chinaskidev <chinaskicharles0@gmail.com>
2025-02-26 17:11:30 -0600

    actualizacion a mi web

--- app.py
+++ app.py
@@ -2,14 +2,14 @@ import streamlit as st
 from PIL import Image
 import json
 from streamlit_lottie import st_lottie
-from app1 import ecoride_page 
+from app1 import agente_page 
 
 
 # Page Setup 
 # Image In Sidebar 
 with st.sidebar.container():
     image = Image.open(r"img/charles.jpg")  
-    st.image(image, use_column_width=True)
+    st.image(image, use_container_width=True)
 
 # Lista de páginas
 paginas = ["Español", "English"]
@@ -38,7 +38,7 @@ def home_page():
 
     # Contenido de la página en español
     if pagina_seleccionada == "Español":
-        st.header("Ciencia de Datos")
+        st.header("¡Hola, bienvenido a mi sitio web!")
         ruta_animacion = "animation/Animation - 1706587782829.json"
         animacion = cargar_animacion(ruta_animacion)
         st_lottie(animacion, speed=1, loop=True)
@@ -46,10 +46,12 @@ def home_page():
         # Contenido adicional en español
         with st.container():
             st.markdown("""
-            ¡Hola, bienvenido a mi sitio web!. Me llamo Juan Carlos V, pero mi familia me llama Carlos o Charles.
-            Aquí encontrarás una muestra de algunos de los proyectos que he desarrollado a lo largo de mi carrera como científico de datos.
-            Cuento con experiencia práctica en una amplia variedad de proyectos, utilizando herramientas como Python, SQL y Google Cloud, entre otras tecnologías del sector. Mi trabajo ha abarcado desde tareas de ETL hasta Ingeniería y Análisis de Datos.
-            Además, poseo conocimientos en Machine Learning y tengo un gran interés en profundizar en áreas como la Inteligencia Artificial y el Deep Learning. Soy una persona apasionada por este campo y estoy convencido de que siempre hay más por aprender. Constantemente busco formas de contribuir con conocimientos y aportar valor a los equipos de trabajo.
+            Soy Juan Carlos V, aunque mi familia me llama Carlos o Charles. Aquí encontrarás una muestra de los proyectos 
+            que he desarrollado a lo largo de mi carrera como Full Stack & Data Engineer. 
+            Poseo experiencia práctica en una amplia variedad de proyectos, utilizando herramientas 
+            como Python, TypeScript, JavaScript, Next.js, React y Google Cloud, entre otras tecnologías.
+            Mi trabajo ha abarcado tareas de ETL, Ingeniería y Análisis de Datos, así como proyectos en Web3. Además, tengo conocimientos en Machine Learning y un gran interés por áreas como la Inteligencia Artificial y el Deep Learning. Soy un apasionado de este campo y estoy convencido de que siempre hay algo nuevo por aprender.
+            Actualmente, me desempeño como Desarrollador en Skinner, una plataforma que emplea IA para el reclutamiento de personal.
             ¡Espero que disfrutes explorando mis proyectos!
             """)
 
@@ -78,7 +80,7 @@ def home_page():
 
     # Contenido de la página en inglés
     elif pagina_seleccionada == "English":
-        st.header("Data Science")
+        st.header("Hello, and welcome to my website!")
         animation_path = "animation/Animation - 1706587782829.json"
         animation = cargar_animacion(animation_path)
         st_lottie(animation, speed=1, loop=True)
@@ -86,11 +88,16 @@ def home_page():
         # Additional content in English
         with st.container():
             st.markdown("""
-            Hello and thank you for visiting my website. My name is Juan Carlos V, but my family calls me Carlos or Charles.
-            Here you will find a sample of some of the projects I have developed throughout my career as a data scientist.
-            I have practical experience in a wide variety of projects, using tools such as Python, SQL, and Google Cloud, among other sector technologies. My work has ranged from ETL tasks to Data Engineering and Analysis.
-            Additionally, I have knowledge in Machine Learning and I have a keen interest in delving into areas such as Artificial Intelligence and Deep Learning. I am passionate about this field and I am convinced that there is always more to learn. 
-            I constantly seek ways to contribute my knowledge and add value to the teams I collaborate with.
+            I am Juan Carlos V, although my family calls me Carlos or Charles. Here you will find a selection
+            of the projects I have developed throughout my career as a Full Stack & Data Engineer. 
+            I have practical experience in a wide range of projects, utilising tools such as Python, 
+            TypeScript, JavaScript, Next.js, React and Google Cloud, among other technologies.
+            My work has encompassed tasks in ETL, Data Engineering and Data Analysis, as well as 
+            projects in Web3. I also have knowledge in Machine Learning and a keen interest in fields such as Artificial 
+            Intelligence and Deep Learning. I am passionate about this area and firmly believe that there is always something new to learn.
+            I am currently employed as a Developer at Skinner, a platform that employs AI for staff recruitment.
+
+            I hope you enjoy exploring my projects!
             """)
 
         # Contact section in English
@@ -170,81 +177,89 @@ def vecinonet_page():
 
 
 # Función para mostrar la página de Steam
-def steam_page():
+def SivarETH_page():
     if pagina_seleccionada == "Español":
-        st.header("Steam")
-        st.write("""Este proyecto se enfoca en el desarrollo de un sistema de recomendación para la plataforma de videojuegos STEAM, donde se ha asumido el rol de MLOps Engineer. 
-                 Se ha implementado un proceso de Extracción, Transformación y Carga (ETL) y un Análisis Exploratorio de Datos (EDA). 
-                 El objetivo principal es desplegar una API con un modelo de Machine Learning capaz de analizar los sentimientos a partir de los comentarios de los usuarios. 
-                 Este modelo también servirá para ofrecer un sistema de recomendación de videojuegos para la plataforma, mejorando la experiencia de los usuario""")
+        st.header("SivarETH-Dapp")
+        st.write("""
+                 El objetivo principal de SivarETH es apoyar a emprendedores y pequeñas fundaciones para beneficiar a personas necesitadas. 
+                 El enfoque de SivarETH se puede describir como una fórmula colaborativa: a + b = 1, 
+                 donde:
+
+                “a” representa a los emprendedores y fundaciones, a quienes educamos en el ecosistema 
+                Web3, ayudándolos a adoptar nuevas tecnologías y estrategias para crecer y adaptarse a la era digital.
+
+                “b” corresponde a los beneficiarios de estos emprendedores, quienes también reciben 
+                educación en Web3, y se les brinda acceso a productos y servicios ofrecidos por ‘a’. 
+                A su vez, “b” puede aprender a usar herramientas descentralizadas, como las criptomonedas, 
+                para mejorar su calidad de vida y acceder a recursos esenciales.
+
+                El modelo SivarETH busca crear un ciclo de impacto mutuo: los emprendedores 
+                (“a”) se capacitan en Web3, mejoran sus negocios y, gracias a las ventas de NFT y 
+                donaciones, se generan fondos para ayudar a los beneficiarios (“b”). 
+                Estos beneficiarios, además de recibir ayuda económica, consumen productos o 
+                servicios de los emprendedores participantes, lo que fortalece las economías 
+                locales y genera sinergia entre ambos grupos. 
+                A largo plazo, tanto “a” como “b” se benefician, no solo económicamente, sino también 
+                a través del aprendizaje y la adopción de nuevas tecnologías.
+
+                Este enfoque garantiza que la tecnología Web3 se utilice no solo como un medio 
+                para recaudar fondos, sino como una herramienta educativa que empodera a todos 
+                los involucrados, creando así un ecosistema inclusivo y sostenible.
+                 """)
 
         # Imagen de  Steam
-        imagen_steam = "img/steam.png"
-        st.image(imagen_steam, use_column_width=True)
+        imagen_SivarETH = "img/sivar.jpg"
+        st.image(imagen_SivarETH, use_container_width=True)
         
-        st.markdown(""""En el proceso del proyecto, se llevó a cabo un Modelo de Recomendación, basado en la similitud del coseno,
-                    donde se crearon las siguientes funciones:""")
-
-        st.markdown("""- Primera Función item-item, introduciendo el id del juego y le devuelve 5 juegos similares al usuario.""")
-
-        st.markdown(""" - Segunda Función de usuario-item, Ingresa el id del usuario y le devuelve 5 juegos recomendados. """)
-                           
-        st.markdown("""Para el primer enfoque del modelo, se establece una relación ítem-ítem. En este escenario, se evalúa un ítem con respecto a su similitud con otros ítems para ofrecer 
-                recomendaciones similares. En este caso, el input corresponde a un juego y el output es una lista de juegos recomendados, 
-                utilizando el concepto de similitud del coseno.""")        
-
-        st.markdown("""Por otra parte, se considera una segunda propuesta para el sistema de recomendación,
-                basada en el filtro user-item. En esta estrategia, se analiza a un usuario para identificar usuarios con gustos similares y se recomiendan 
-                ítems que hayan sido apreciados por estos usuarios afines.""")   
         
-        # Enlace a video    
-        st.write("Puedes ver el Deploy en [Video](https://www.loom.com/share/8cfd463c84d24f748390800dc3718fbb?sid=4625d89b-43ee-4693-89ec-193e66c28301)")
         # Enlace a Github
-        st.write("Lee más en [Github](https://github.com/Chinaskidev/STEAM_Recomendacion)")
+        st.write("Lee más en [Github](https://github.com/Chinaskidev/Dapp-SivarETH)")
         
         
     elif pagina_seleccionada == "English":
-        st.header("Steam")
-        st.write("""This project focuses on the development of a recommended system for the STEAM video game platform, 
-                 where the role of MLOps Engineer has been assumed. An Extraction, 
-                 Transformation and Load (ETL) process and an Exploratory Data Analysis (EDA) 
-                 have been implemented. The main objective is to deploy an API with a Machine Learning model, 
-                 capable of analyzing sentiments from user comments. 
-                 This model will also serve to offer a video game recommended system for the platform, 
-                 which will lead to improving the user experience.""")
+        st.header("SivarETH-Dapp")
+        st.write("""
+                 SivarETH main objective is to support entrepreneurs and small foundations in order 
+                 to benefit people in need. SivarETH's approach can be described as a collaborative 
+                 formula: a + b = 1, where:
+
+                “a” represents entrepreneurs and foundations, whom we educate in the Web3 ecosystem, 
+                helping them adopt new technologies and strategies to grow and adapt to the digital age.
+
+                “b” corresponds to the beneficiaries of these entrepreneurs, who also receive 
+                education in Web3, and are provided access to products and services offered by ‘a’. 
+                In turn, “b” can learn to use decentralized tools, such as cryptocurrencies, 
+                to improve their quality of life and access essential resources.
+
+                The SivarETH model seeks to create a cycle of mutual impact: entrepreneurs 
+                (“a”) are trained in Web3, improve their businesses and, thanks to the sales of 
+                NFTs and donations, funds are generated to help beneficiaries (“b”). 
+                These beneficiaries, in addition to receiving financial assistance, 
+                consume products or services from the participating entrepreneurs, 
+                which strengthens local economies and generates synergy between the two groups. 
+                In the long run, both “a” and “b” benefit, not only economically, 
+                but also through learning and adopting new technologies.
+
+                This approach ensures that Web3 technology is used not only as a means of 
+                fundraising, but as an educational tool that empowers all involved, thus creating 
+                an inclusive and sustainable ecosystem.
+                 
+                 """)
         
         # Imagen de Steam en inglés
-        imagen_steam = "img/steam.png"
-        st.image(imagen_steam, use_column_width=True)
+        imagen_SivarETH = "img/sivar.jpg"
+        st.image(imagen_SivarETH, use_container_width=True)
         
-        # Additional content in English
-        st.markdown("""In the project process, a Recommendation Model 
-                    was implemented based on cosine similarity, where the following functions were created:""")
         
-        st.markdown("""- First Function item-item, inputting the game ID and returning 5 similar games to the user.""")
-
-        st.markdown("""- Second user-item Function, inputs the user ID and returns 5 recommended games.""")
-        
-        st.markdown("""For the first model approach, an item-item relationship is established. 
-                    In this scenario, an item is evaluated with respect to its similarity to other items to provide similar recommendations. 
-                    In this case, the input corresponds to a game, and the output is a list of recommended games, using the concept of cosine similarity.
-                    """)   
-        
-        st.markdown("""On the other hand, a second proposal for the recommendation system is considered, based on the user-item filter. 
-                    Using this method, a user is analyzed to identify other users with similar tastes and items that have been recommended by these similar users.  
-                    When a player finishes playing a game, the platform will recommend other games that were chosen by other users.""") 
-        
-        # Enlace a video    
-        st.write("You can see the deployment at [Video](https://www.loom.com/share/8cfd463c84d24f748390800dc3718fbb?sid=4625d89b-43ee-4693-89ec-193e66c28301)")
         # Enlace a Github
-        st.write("Read more on [Github](https://github.com/Chinaskidev/STEAM_Recomendacion)")      
+        st.write("Read more on [Github](https://github.com/Chinaskidev/Dapp-SivarETH)")      
 
 
 # Obtener el estado de la sesión
 state = get_state()
 
 # Mostrar el selector de página en la barra lateral
-pagina_adicional_seleccionada = st.sidebar.selectbox("Proyectos/Projects", ["Home", "VecinoNet", "Steam", "ECORIDE-NYC"])
+pagina_adicional_seleccionada = st.sidebar.selectbox("Proyectos/Projects", ["Home", "VecinoNet", "SivarETH", "Agente-ElizaOs"])
 
 # Actualizar el estado de la página
 state.page = pagina_adicional_seleccionada
@@ -254,8 +269,8 @@ if state.page == 'Home':
     home_page()
 elif state.page == 'VecinoNet':
     vecinonet_page()
-elif state.page == 'Steam':
-    steam_page()
-elif state.page == 'ECORIDE-NYC':
-    ecoride_page(pagina_seleccionada)
+elif state.page == 'SivarETH':
+    SivarETH_page()
+elif state.page == 'Agente-ElizaOs':
+    agente_page(pagina_seleccionada)
 
--- app1.py
+++ app1.py
@@ -2,66 +2,56 @@
 import streamlit as st
 
 # Función para mostrar la página Dataminds
-def ecoride_page(pagina_seleccionada):
+def agente_page(pagina_seleccionada):
     if pagina_seleccionada == "Español":
-        st.header("ECORIDE-NYC")
-        st.write("""En la actualidad, los servicios de taxis y viajes compartidos en la ciudad de Nueva York han emergido como alternativas populares al transporte público. 
-                 Estos servicios no solo proporcionan una valiosa fuente de datos sobre la ubicación de los vehículos, 
-                 sino también información relevante sobre la calificación de los conductores, 
-                 las ganancias diarias y mensuales de los taxis convencionales en diversos boroughs de la ciudad de Nueva York, 
-                 así como la autonomía, eficiencia y costos asociados a los autos eléctricos. 
-                 El análisis de estos datos puede ofrecer percepciones clave sobre los cambios en los patrones de viaje, 
-                 particularmente en el contexto de la evolución postpandemia y la creciente adopción de modelos de trabajo flexibles.""")
+        st.header("Agente-en-X-ElizaOs")
+        st.write("""
+                 Construí un agente sencillo que te habla sobre Blockchain y Web3 utilizando ElizaOS, 
+                 un framework que te permite crear tus propios agentes de inteligencia artificial.
+                Este agente está diseñado para responder preguntas y ofrecer explicaciones sobre 
+                conceptos clave en el ecosistema de la Web3, desde la tecnología Blockchain hasta 
+                criptomonedas y contratos inteligentes.
+                Con ElizaOS, puedes desarrollar agentes personalizados que se ajusten a tus 
+                necesidades, integrando múltiples modelos de IA y diferentes plataformas. 
+                ¡Es una herramienta poderosa para construir tus propios agentes!
+                 
+                 """)
 
-        # Imagen de ecoride
-        imagen_eco = "img/taxi.jpg"
-        st.image(imagen_eco, use_column_width=True)
         
-        st.markdown("""En este proyecto se desarrollo un análisis integral de datos donde se aplicaron técnicas de Machine Learning para evaluar 
-                    la viabilidad y el impacto de la incorporación de vehículos eléctricos en la flota, orientado a mejorar la sostenibilidad y eficiencia del transporte urbano. 
-                    Este proyecto tiene como **objetivo general** proporcionar a una empresa de transporte de pasajeros de micros de media y larga distancia 
-                    información clave sobre la posible transición a vehículos eléctricos, considerando aspectos como la reducción de la huella ambiental, 
-                    el análisis de costos y ganancias, así como la implementación de estrategias sostenibles.""")
         
-         # Imagen de ML
-        imagen_eco = "img/app.png"
-        st.image(imagen_eco, use_column_width=True)
+        # Imagen de agente
+        imagen_agente = "img/eliza_diagram.png"
+        st.image(imagen_agente, use_container_width=True)
         
-        # Enlace a App
-        st.write("Puedes probar el Modelo en [ML](https://demandpredictor-argsx2hozvnjcd7wlxfqg9.streamlit.app/)")
+        
+        
+         
         
         # Enlace a Github
-        st.write("Lee más en [Github](https://github.com/Chinaskidev/Dataminds-Consulting)")
+        st.write("Lee más en [Github](https://github.com/Chinaskidev/Agente-en-X-ElizaOs)")
 
         
         
     elif pagina_seleccionada == "English":
-        st.header("ECORIDE-NYC")
-        st.write("""Currently, taxi and ridesharing services in New York City have emerged as popular alternatives to public transportation. 
-                 These services not only provide valuable data on vehicle locations but also relevant information on driver ratings,
-                 daily and monthly earnings of conventional taxis in various boroughs of New York City, as well as the autonomy, 
-                 efficiency, and costs associated with electric vehicles. 
-                 Analyzing this data can offer key insights into changes in travel patterns, particularly in the context of post-pandemic 
-                 evolution and the growing adoption of flexible work models.""")
+        st.header("Agent-in-X-ElizaOs")
+        st.write("""
+                 I built a simple agent that talks to you about Blockchain and Web3 using 
+                 ElizaOS, a framework that lets you create your own artificial intelligence agents. 
+                 This agent is designed to answer questions and provide explanations on key concepts 
+                 within the Web3 ecosystem, from blockchain technology to cryptocurrencies and smart 
+                 contracts. With ElizaOS, you can develop custom agents tailored to your needs, 
+                 integrating multiple AI models and different platforms. It's a powerful tool for 
+                 exploring decentralisation and the future of the web!
+                 
+                 """)
         
-        # Imagen de VecinoNet en inglés
-        imagen_ecoride = "img/taxi.jpg"
-        st.image(imagen_ecoride, use_column_width=True)
         
-        st.markdown("""In this project, a comprehensive data analysis was conducted applying Machine Learning techniques 
-                    to assess the feasibility and impact of incorporating electric vehicles into the fleet, 
-                    aimed at enhancing the sustainability and efficiency of urban transportation. 
-                    The main objective of this project is to provide a long-distance passenger transport company with key insights into 
-                    the potential transition to electric vehicles, considering aspects such as reducing environmental footprint, 
-                    cost-benefit analysis, and the implementation of sustainable strategies.""")
         
-        # Imagen de ML
-        imagen_eco = "img/app.png"
-        st.image(imagen_eco, use_column_width=True)
+        # Imagen de agente
+        imagen_agente = "img/eliza_diagram.png"
+        st.image(imagen_agente, use_container_width=True)
         
-        # Enlace a App
-        st.write("You can test the model on [ML](https://demandpredictor-argsx2hozvnjcd7wlxfqg9.streamlit.app/)")
         
         # Enlace a Github
-        st.write("Read more on [Github](https://github.com/Chinaskidev/Dataminds-Consulting)")
+        st.write("Read more on [Github](https://github.com/Chinaskidev/Agente-en-X-ElizaOs)")
         
